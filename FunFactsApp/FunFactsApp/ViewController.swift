55641ef6bf723765e373843487becc74c247b132
tmwalker.nyc@gmail.com <clard1970@gmail.com>
2016-06-07 15:11:34 -0400

    First Commit

---
+++ FunFactsApp/FunFactsApp/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  FunFactsApp
+//
+//  Created by TB2 Dumputer on 8/13/15.
+//  Copyright (c) 2015 NordikLabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var funFactLabel: UILabel!
+    @IBOutlet weak var funFactButton: UIButton!
+    
+    let factBook = FactBook()
+    let colorWheel = ColorWheel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        funFactLabel.text = factBook.randomFact()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func showFunFact() {
+        var randomColor = colorWheel.randomColor()
+        view.backgroundColor = randomColor
+        funFactButton.tintColor = randomColor
+        funFactLabel.text = factBook.randomFact()
+    }
+}
+
---
+++ RandomGenerators.playground/Contents.swift
@@ -0,0 +1,21 @@
+// Random Generators
+
+import UIKit
+
+
+// Random Numbers
+
+var randomNumber = Int(arc4random_uniform(10))
+
+
+// UIColor Random Colors
+
+var redColor = UIColor(red: 223/255.0, green: 86/255.0, blue: 94/255.0, alpha: 1.0)
+
+
+let colorsArray = [redColor]
+
+
+
+
+
